ddf14e96f44ae4a79b879cede867417e932164d8
Gonzalo Jiménez Hinchado <gonzalojimenezhinchado@MacBook-Pro-de-Gonzalo.local>
2020-11-27 00:58:15 +0100

    Trying to deploy on heroku

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -4,13 +4,9 @@ import connexion
 
 from swagger_server import encoder
 
+app = connexion.App(__name__, specification_dir='./swagger/')
+app.app.json_encoder = encoder.JSONEncoder
+app.add_api('swagger.yaml', arguments={'title': 'Notification Management API'}, pythonic_params=True)
 
-def main():
-    app = connexion.App(__name__, specification_dir='./swagger/')
-    app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('swagger.yaml', arguments={'title': 'Notification Management API'}, pythonic_params=True)
-    app.run(port=8000)
-
-    return app
-
-app = main()
+if __name__ == "__main__":
+    app.run(threaded=True, port=5000)
