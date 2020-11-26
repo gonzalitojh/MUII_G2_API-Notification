dc9134d84bd2313da51d9f6241e37f9ac84ec1dc
Gonzalo Jiménez Hinchado <gonzalojimenezhinchado@MacBook-Pro-de-Gonzalo.local>
2020-11-26 13:15:23 +0100

    Done the Notification API.

--- swagger_server/controllers/notification_controller.py
+++ swagger_server/controllers/notification_controller.py
@@ -5,8 +5,10 @@ from swagger_server.models.notification import Notification  # noqa: E501
 from swagger_server.models.one_notification import OneNotification  # noqa: E501
 from swagger_server import util
 
+from swagger_server.static.constants import *
 
-def send_notification_all(body):  # noqa: E501
+
+def send_notification_all(visit):  # noqa: E501
     """Send notification to all the people close to the user
 
     Send notification to all the people close to the user # noqa: E501
@@ -17,11 +19,15 @@ def send_notification_all(body):  # noqa: E501
     :rtype: str
     """
     if connexion.request.is_json:
-        body = Notification.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        visit = Notification.from_dict(connexion.request.get_json())  # noqa: E501
+        if visit.known:
+            known_visitor = 'known and his/her name is ' + visit.name
+        else:
+            known_visitor = 'not known'
+    return MESSAGE_TO_ALL_LEGITIMATE_PERSON.format(known_visitor)
 
 
-def send_notification_one_person(body):  # noqa: E501
+def send_notification_one_person(visit):  # noqa: E501
     """Send notification to one relative of the user
 
     Send notification to one relative of the user # noqa: E501
@@ -32,11 +38,15 @@ def send_notification_one_person(body):  # noqa: E501
     :rtype: str
     """
     if connexion.request.is_json:
-        body = OneNotification.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        visit = OneNotification.from_dict(connexion.request.get_json())  # noqa: E501
+        if visit.known:
+            known_visitor = 'known and his/her name is ' + visit.name
+        else:
+            known_visitor = 'not known'
+    return MESSAGE_TO_ONE_LEGITIMATE_PERSON.format(visit.device_mac, known_visitor)
 
 
-def send_notification_user(body):  # noqa: E501
+def send_notification_user(visit):  # noqa: E501
     """Send notification to the user
 
     Send notification to the user # noqa: E501
@@ -47,5 +57,9 @@ def send_notification_user(body):  # noqa: E501
     :rtype: str
     """
     if connexion.request.is_json:
-        body = Notification.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        visit = Notification.from_dict(connexion.request.get_json())  # noqa: E501
+        if visit.known:
+            known_visitor = 'known and his/her name is ' + visit.name
+        else:
+            known_visitor = 'not known'
+    return MESSAGE_TO_USER.format(known_visitor)
