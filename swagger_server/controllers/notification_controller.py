e07f382daec40ec6a8eea350f4c44aae79cf84b4
SergioFdezRc <s.fernandez.rincon@gmail.com>
2020-11-29 21:16:49 +0100

    Se ha modificado el tipo de dato que retornan las funciones

--- swagger_server/controllers/notification_controller.py
+++ swagger_server/controllers/notification_controller.py
@@ -1,5 +1,6 @@
 import connexion
 import six
+from flask import jsonify
 
 from swagger_server.models.notification import Notification  # noqa: E501
 from swagger_server.models.one_notification import OneNotification  # noqa: E501
@@ -18,13 +19,14 @@ def send_notification_all(visit):  # noqa: E501
 
     :rtype: str
     """
+    known_visitor = ""
     if connexion.request.is_json:
         visit = Notification.from_dict(connexion.request.get_json())  # noqa: E501
         if visit.known:
             known_visitor = 'known and his/her name is ' + visit.name
         else:
             known_visitor = 'not known'
-    return MESSAGE_TO_ALL_LEGITIMATE_PERSON.format(known_visitor)
+    return jsonify({"msesage": MESSAGE_TO_ALL_LEGITIMATE_PERSON.format(known_visitor)})
 
 
 def send_notification_one_person(visit):  # noqa: E501
@@ -32,18 +34,19 @@ def send_notification_one_person(visit):  # noqa: E501
 
     Send notification to one relative of the user # noqa: E501
 
-    :param body: 
+    :param body:
     :type body: dict | bytes
 
     :rtype: str
     """
+    known_visitor = ""
     if connexion.request.is_json:
         visit = OneNotification.from_dict(connexion.request.get_json())  # noqa: E501
         if visit.known:
             known_visitor = 'known and his/her name is ' + visit.name
         else:
             known_visitor = 'not known'
-    return MESSAGE_TO_ONE_LEGITIMATE_PERSON.format(visit.person_phone, known_visitor)
+    return jsonify({"msesage": MESSAGE_TO_ONE_LEGITIMATE_PERSON.format(visit.person_phone, known_visitor)})
 
 
 def send_notification_user(visit):  # noqa: E501
@@ -51,15 +54,16 @@ def send_notification_user(visit):  # noqa: E501
 
     Send notification to the user # noqa: E501
 
-    :param body: 
+    :param body:
     :type body: dict | bytes
 
     :rtype: str
     """
+    known_visitor = ""
     if connexion.request.is_json:
         visit = Notification.from_dict(connexion.request.get_json())  # noqa: E501
         if visit.known:
             known_visitor = 'known and his/her name is ' + visit.name
         else:
             known_visitor = 'not known'
-    return MESSAGE_TO_USER.format(known_visitor)
+    return jsonify({"msesage": MESSAGE_TO_USER.format(known_visitor)})
